393e3009ac5966db848a9bcc0621381461021d5d
m1khah <alexeyenko92@gmail.com>
2020-03-28 15:14:43 +0200

    Added calculator

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,9 +2,9 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.Scanner;
 
 public abstract class App {
-
 
     public static void main(String[] args) {
         names("John");
@@ -16,6 +16,7 @@ public abstract class App {
         sumArray(new int[]{3, 4, 5});
         sumArray1(new int[]{3, 4, 5});
         sumArray2(new int[]{3, 4, 5});
+        calculator();
     }
 
     public static void names(String input) {
@@ -38,24 +39,23 @@ public abstract class App {
         String result = "Number is ";
         if (a % 2 == 0) {
             result += "even";
-        }
-        else  {
+        } else {
             result += "odd";
         }
         System.out.println(result);
     }
 
-    public static void fiveToOne(){
-        for (int x = 5; x >= 1; x--){
+    public static void fiveToOne() {
+        for (int x = 5; x >= 1; x--) {
             System.out.print(x + " ");
         }
         System.out.print("\n");
     }
 
     public static void table(int x) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        for(int num : arr) {
-            System.out.println(x + " * " + num + " = " + num*x);
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int num : arr) {
+            System.out.println(x + " * " + num + " = " + num * x);
         }
     }
 
@@ -80,10 +80,31 @@ public abstract class App {
         System.out.println("The sum is " + sum + " in even more educational way");
     }
 
-    public static  void calculator(int[] args) {
-
+    public static void calculator() {
+        Scanner scan = new Scanner(System.in);
+        int result;
+        System.out.println("Enter first number");
+        int num1 = scan.nextInt();
+        System.out.println("Enter operation");
+        char operation = scan.next().charAt(0);
+        System.out.println("Enter second number");
+        int num2 = scan.nextInt();
+        switch (operation) {
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + operation);
+        }
+    System.out.println("Result:" + result);
     }
-//    1. Убрать .idea, iml, target из проекта. если пока не знаешь как - сделаешь это после лекции Сережи по гиту.
-//3. Task 3: попробуй написать свою сортировку, не используя метод sort();
-//4. Task 6: почему присутствует закоментированый код? можно сделать проще с обычным циклом for, подумай как
 }
